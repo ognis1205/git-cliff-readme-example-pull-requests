6a447b06e2d0805a30e225478872de51047d3db3
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-06-16 12:17:08 +0900

    refactor(parser): expose string functions
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

---
+++ src/parser.rs
@@ -0,0 +1,9 @@
+#[allow(dead_code, unused_variables)]
+pub fn parse_number(input: impl AsRef<str>) -> ! {
+    unimplemented!()
+}
+
+#[allow(dead_code, unused_variables)]
+pub fn parse_string(input: impl AsRef<str>) -> ! {
+    unimplemented!()
+}
