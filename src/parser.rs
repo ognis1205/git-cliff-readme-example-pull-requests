2a3e6ae702fe992bd0b19fa01856ca3bdacaa082
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-06-16 14:43:37 +0900

    feat(parser): add ability to parse arrays
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -7,3 +7,8 @@ pub fn parse_number(input: impl AsRef<str>) -> ! {
 pub fn parse_string(input: impl AsRef<str>) -> ! {
     unimplemented!()
 }
+
+#[allow(dead_code, unused_variables)]
+pub fn parse_array(input: impl AsRef<str>) -> ! {
+    unimplemented!()
+}
